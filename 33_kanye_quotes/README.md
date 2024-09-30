adb6e91ee16c715f30c5bf0879ff9a8388b71e48
Thomas Kuryura <thomas.adm@gmail.com>
2024-09-30 18:09:05 -0300

    Finising 33_kayne_quotes

---
+++ 33_kanye_quotes/README.md
@@ -0,0 +1,22 @@
+# Birthday wisher
+
+This is a simple program that check a csv list of birthdays and emails and send a random "happy birthday" e-mail.
+
+
+This challenge teaches the concepts of:
+
+ - api requests
+
+## Setup
+
+This program uses the Python Tkinter and requests
+
+```
+pip install requests
+```
+
+## How to run
+
+```
+python main.py
+```
--- 33_kanye_quotes/main.py
+++ 33_kanye_quotes/main.py
@@ -1,11 +1,13 @@
 from tkinter import *
+import requests
 
+API_URL = "https://api.kanye.rest/"
 
 def get_quote():
-    pass
-    #Write your code here.
-
-
+    response = requests.get(API_URL)
+    response.raise_for_status()
+    data = response.json()
+    canvas.itemconfig(quote_text, text=data["quote"])
 
 window = Tk()
 window.title("Kanye Says...")
@@ -14,7 +16,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
