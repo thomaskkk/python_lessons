e90ad5e09b2ae282701dd010bde3ea7915db3a7c
Thomas Kuryura <thomas.adm@gmail.com>
2024-11-26 09:21:21 -0300

    Finishing day 63 library project

--- 63_library_project/main.py
+++ 63_library_project/main.py
@@ -1,31 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
+
+class Base(DeclarativeBase):
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
 
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    with app.app_context():
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = result.scalars().all()
+
+    return render_template("index.html", all_books=all_books, book_qnty=len(all_books))
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        with app.app_context():
+            new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+            db.session.add(new_book)
+            db.session.commit()
+        
+        return redirect(url_for("home"))
+    
+    else:
+        return render_template("add.html")
 
 
 if __name__ == "__main__":
